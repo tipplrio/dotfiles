8197e4ddb0ab3db118e34edb3d5fd23646a122bb
James G. Kim <jgkim@jayg.org>
2016-04-22 11:43:44 +0900

    Updated `update.sh`

--- update.sh
+++ update.sh
@@ -2,4 +2,20 @@
 
 echo "Making the environment up-to-date."
 cd ~/.ansible
-ansible-playbook -K -i hosts site.yml
+
+# Ask for the administrator password upfront.
+if [ "$(whoami)" != "root" ]; then
+  sudo -v
+
+  # Keep-alive: update existing `sudo` time stamp until the script has finished.
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
+
+OPTIONS=''
+if [ $@ ]; then
+  OPTIONS=$@
+fi
+
+ansible-playbook site.yml -i hosts ${OPTIONS}
+
+unset OPTIONS

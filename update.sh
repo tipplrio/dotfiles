b94268942242f29a1d452a177bfb8d2ca9380543
James G. Kim <jgkim@jayg.org>
2016-05-11 06:40:09 +0900

    Fix a bug in `update.sh` and tweak to avoid timeout
    See also: https://docs.travis-ci.com/user/customizing-the-build/#Build-Timeouts

--- update.sh
+++ update.sh
@@ -3,16 +3,8 @@
 echo "Making the environment up-to-date."
 cd ~/.ansible
 
-# Ask for the administrator password upfront.
-if [ "$(whoami)" != "root" ]; then
-  sudo -v
-
-  # Keep-alive: update existing `sudo` time stamp until the script has finished.
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-fi
-
 OPTIONS='-i hosts'
-if [ -z "$@" ]; then
+if [ -z "$*" ]; then
   OPTIONS="${OPTIONS} --ask-become-pass"
 fi
 
